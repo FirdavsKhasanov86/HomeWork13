f5d01a72598b51b5ccb96f2816b14849543853a0
user <user@Williams-iMac.local>
2020-04-17 19:24:25 +0500

    Added method Pop()

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace HomeWork13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Medthood Pop
+            string[] numbers = { "1", "2", "3", "4", "5" };
+            
+            foreach (var y in numbers)
+            {
+                Console.WriteLine(y);
+            }
+            Console.WriteLine("------------------------------");
+            ArrayHelper<string> ar = new ArrayHelper<string>();
+            ar.Pop(ref numbers);
+            foreach (var y in numbers)
+            {
+                Console.WriteLine(y);
+            }
+
+        }
+    }
+
+    class ArrayHelper<T>
+    {
+        public T Pop(ref T[] stringArr)
+        {
+
+            T[] arr = new T[stringArr.Length - 1];
+            for (int i = 0; i < stringArr.Length - 1; i++)
+            {
+                arr[i] = stringArr[i];
+
+            }
+            T a = stringArr[stringArr.Length - 1];
+            stringArr = arr;
+            return a;
+        }
+    }
+}
